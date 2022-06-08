d720d00efb88fd681e15510dbfb4483176333977
Jesus Enrique Aldana Sanchez <jealdana@gmail.com>
2022-06-08 14:10:13 -0500

    Update passport.js
    Actualizacion para que funcione el uso del token

--- config/passport.js
+++ config/passport.js
@@ -3,22 +3,28 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const Keys = require('./keys');
 const User = require('../models/user');
 
-
 module.exports = (passport) => {
-    let opts ={};
-    opts.jwtFromRequest= ExtractJwt.fromAuthHeaderWithScheme('jwt');
-    opts.secretOrkey= Keys.secretOrkey;
-    passport.use( new JwtStrategy(opts, (jwt_payload,done) => {
+
+    let opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    opts.secretOrKey = Keys.secretOrKey;
+
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    
         User.findById(jwt_payload.id, (err, user) => {
-            if (err){
-                return done(err,false);
+
+            if (err) {
+                return done(err, false);
             }
-            if (user){
-                return done(null,user);
+            if (user) {
+                return done(null, user);
             }
-            else{
-                return done(null,false);
+            else {
+                return done(null, false);
             }
+
         });
+
     }));
+
 }
